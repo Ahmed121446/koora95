50e4c70747631dce48fc28e55e8af9d01c1029c9
Apple <apple@Apples-MacBook-Pro-2.local>
2017-12-04 14:20:29 +0200

    make continent relation with country

--- app/continent.php
+++ app/continent.php
@@ -6,5 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class continent extends Model
 {
-    //
+    public function Countries(){
+    	// continent has many Countries   ---->   1-M    relationship
+    	return $this->hasMany(\App\Country::class);
+    }
 }
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel lol
+                    Laravel 
                 </div>
 
                 <div class="links">
