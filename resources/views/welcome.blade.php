9ee9518642960a25817a62a6e95f7f8acca1a41c
abdulRahmanAbouzaid <abdarrahman.abouzaid@gmail.com>
2017-12-04 14:11:44 +0200

    test Abouzaid branch

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Koora95
                 </div>
 
                 <div class="links">
