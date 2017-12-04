5e8a93fc22757328d09b0e479e570712703804a6
Apple <apple@Apples-MacBook-Pro-2.local>
2017-12-04 14:11:26 +0200

    test use my branch

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Laravel lol
                 </div>
 
                 <div class="links">
