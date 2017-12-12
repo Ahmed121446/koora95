962fd2c0a9d888c7783a44a6469b2a80f089d680
abdulRahmanAbouzaid <abdarrahman.abouzaid@gmail.com>
2017-12-12 16:04:38 +0200

    confirm match result

--- app/Http/Controllers/MatchesController.php
+++ app/Http/Controllers/MatchesController.php
@@ -7,27 +7,39 @@ use App\Http\Requests\MatchRequest;
 use Illuminate\Http\Request;
 use App\Match;
 use App\Season;
-use App\Week;
-use App\Round;
 use App\Team;
+use Carbon\Carbon;
 
 class MatchesController extends Controller
 {
     public function getSeasonMatches(Season $season)
     {
+    	if(!$season->id){
+    		return response()->json(['message' => 'Season Not Found'], 404);
+    	}
+
     	$matches = $season->matches;
-     	return $matches;
+     	return response()->json(['data' => $matches], 200);
     }
 
 
     public function addMatch(Season $season, MatchRequest $request)
     {
+    	if(!$season->active){
+    		return response()->json(['message' => 'Season is inactive'], 404);
+    	}
     	$match = $request->add($season);
     	return response()->json(['data' => $match],201);
     }
 
 
-    public function update(Request $request, Season $season, Match $match){
+    public function update(Request $request, Season $season, Match $match)
+    {
+
+    	if(!$season->active){
+    		return response()->json(['message' => 'Season is inactive'], 404);
+    	}
+
     	$match = $season->matches()->find($match->id);
 
     	$match->update($request->all());
@@ -49,6 +61,7 @@ class MatchesController extends Controller
     	],204);
     }
 
+
     public function findByStage(Season $season, $stage_id)
     {
     	$stage = $season->stages()->find($stage_id);
@@ -56,9 +69,9 @@ class MatchesController extends Controller
     	return $matches;
     }
 
-    public function Find_Date(Season $season,$date){
 
-
+    public function Find_Date(Season $season,$date)
+    {
         $matches = $season->matches;
         $matchs = $matches->where('date',$date);
         if (!$matchs->first()) {
@@ -73,7 +86,9 @@ class MatchesController extends Controller
             ],200); 
     }
 
-    public function Find_Team_Matches(Season $season,Team $team){
+
+    public function Find_Team_Matches(Season $season,Team $team)
+    {
         $team_in_season = $season->registeredTeams()->where('team_id',$team->id)->first();
         if (!$team_in_season) {
             return response()->json([
@@ -95,6 +110,92 @@ class MatchesController extends Controller
                 'matches' => $team_matches
             ],200);
         }
+    }
+
+
+    public function findAllByDate($date)
+    {
+    	$matches = Match::where('date',$date)->get();
+
+        if (!$matches->first()) {
+            return response()->json([
+                'Message' => 'there is no matches in this date'
+            ],404);
+        }
+
+        return response()->json([
+                'data' => $matches
+            ],200); 
+    }
+
+
+
+    public function todayMatches()
+    {
+    	$today = Carbon::now();
+    	$today = $today->toDateString();
+
+
+    	$matches = Match::where('date',$today)->get();
+
+        if (!$matches->first()) {
+            return response()->json([
+                'Message' => 'there is no matches in this date'
+            ],404);
+        }
+
+        return response()->json([
+                'data' => $matches
+            ],200);
+    }
+
+
+    public function confirmResult(Season $season, Match $match, Request $request)
+    {
+        $first_team = $match->register_team_1_id;
+        $second_team = $match->register_team_2_id;
+
+        $first_team = $season->registeredTeams()->find($first_team);
+        $second_team = $season->registeredTeams()->find($second_team);
+
+        $match->is_played = true;
+        $first_team->played += 1;
+        $second_team->played += 1;
+
+        $match->team_1_goals = $request->get('first_team_goals');
+        $match->team_2_goals = $request->get('second_team_goals');
+        
+        if($match->team_1_goals > $match->team_2_goals){
+           
+            $match->winner_id = $match->register_team_1_id;
+            $first_team->wins += 1;
+            $first_team->points += 3;
+            $second_team->losses += 1;
+
+        }else if($match->team_1_goals < $match->team_2_goals){        
+           
+            $match->winner_id = $match->register_team_2_id;
+            $second_team->wins += 1;
+            $second_team->points += 3;
+            $first_team->losses += 1;
+
+        }else{
+            $match->winner_id = 0;
+            $first_team->draws += 1;
+            $second_team->draws += 1;
+            $first_team->points += 1;
+            $second_team->points += 1;
+        }
+
+        $first_team->goals_for += $match->team_1_goals;
+        $first_team->goals_against += $match->team_2_goals;
+        $second_team->goals_for += $match->team_2_goals;
+        $second_team->goals_against += $match->team_1_goals;
+
+        $match->save();
+        $first_team->save();
+        $second_team->save();
+
     }
 
 }
