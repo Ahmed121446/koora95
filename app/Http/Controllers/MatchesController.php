e05354548efb536303abcc0e7b0b31e56a6b26fe
Apple <apple@Apples-MacBook-Pro-2.local>
2017-12-31 10:24:00 +0200

    update middleware

--- app/Http/Controllers/MatchesController.php
+++ app/Http/Controllers/MatchesController.php
@@ -25,7 +25,7 @@ use App\competition;
 class MatchesController extends Controller
 {
     public function __construct(){
-        $this->middleware('auth');
+       $this->middleware('auth')->except(['Get_Today_Matches_View']);
     }
     public function getTeams(Request $request){
 
